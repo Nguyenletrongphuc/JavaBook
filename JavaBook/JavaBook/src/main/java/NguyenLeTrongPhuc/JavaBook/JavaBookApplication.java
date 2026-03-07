f77a160554024e6517b35b01f965c61f11c50329
Nguyenletrongphuc <ecchiphuc@gmail.com>
2026-03-07 21:38:26 +0700

    khoi tao

---
+++ JavaBook/JavaBook/src/main/java/NguyenLeTrongPhuc/JavaBook/JavaBookApplication.java
@@ -0,0 +1,13 @@
+package NguyenLeTrongPhuc.JavaBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBookApplication.class, args);
+	}
+
+}
---
+++ JavaBook/JavaBook/src/test/java/NguyenLeTrongPhuc/JavaBook/JavaBookApplicationTests.java
@@ -0,0 +1,13 @@
+package NguyenLeTrongPhuc.JavaBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
